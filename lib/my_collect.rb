ec2bbcaf32f9cec54a6a1dea8ff1732ffd93b411
Haoshuang Wang <haoshuang@flatironschool.com>
2017-08-12 17:36:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+require 'pry'
 
+def my_collect(list)
+  i = 0
+  collection = []
+  while i < list.length
+    collection << yield(list[i])
+    i += 1
+  end
+  collection
+end
 
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
+  # binding.pry
+  name.split(" ").first
+end
+
+my_collect(["ruby", "javascript", "python"]) do |language|
+  # binding.pry
+  language.upcase
+end
